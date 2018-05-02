1d875269b6ec609feaf491013381f94ad2ada913
sethuchakkra <sethuchakkra@gmail.com>
2018-05-02 23:34:14 +0530

    New file

---
+++ test1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+	int arr[5]={8,9,7,3,4};
+	int temp,i,j;
+	for(i=0;i<4;i++)
+		for(j=i;j<4;j++)
+		{
+			if(arr[i]>a[j])
+			{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+			}
+		}
+}
