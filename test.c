3f8c87ddc33d368a514144eb6d25299665bf3500
sethuchakkra <sethuchakkra@gmail.com>
2018-05-11 00:31:29 +0530

    comment corrected

--- test.c
+++ test.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+
 #define fun(x)(printf("%d",x))
+
 void main()
 {
+
 int a,b;
 
 int sum,sub,mul,div;
 
 /*Addition*/
-sum=a+b;/*add two numbers a and b */
+sum=a+b;
+
 /*Subraction*/
 sub=a-b;
 
@@ -17,6 +21,8 @@ mul=a*b;
 /*Division*/
 div=a/b;
 
+/*Call the function*/
 fun(sum);
+
 }
 
